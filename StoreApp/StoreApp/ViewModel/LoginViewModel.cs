0dbc983372c9b98f178732ae372dae53428a2dfc
Пользователь <zulfugarovfarhad6@gmail.com>
2023-03-08 15:16:50 +0400

    Problem

---
+++ StoreApp/StoreApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.ViewModel
+{
+    internal class LoginViewModel
+    {
+        public IMessenger Messenger { get; set; } = new Messenger();
+        public RelayCommand LoginCommand
+        {
+            get => new(() =>
+            { 
+                Messenger.Send(typeof(LoginViewModel));
+            });
+        }
+
+        public RelayCommand RegisterCommand
+        {
+            get => new(() =>
+            {
+                Messenger.Send(typeof(RegistrationViewModel));
+            });
+        }
+    }
+}
---
+++ StoreApp/StoreApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using StoreApp.Model;
+
+namespace StoreApp.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        IMessenger Messenger = new Messenger();
+        public ViewModelBase CurrentViewModel { get; set; } = new LoginViewModel();
+    }
+}
