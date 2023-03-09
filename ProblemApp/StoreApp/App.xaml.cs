3333f3b01eeba6c217c6b82678c02070fe18a201
Пользователь <zulfugarovfarhad6@gmail.com>
2023-03-10 01:09:06 +0400

    Added App With Problem

---
+++ ProblemApp/StoreApp/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using SimpleInjector;
+using StoreApp.View;
+using StoreApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StoreApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Container Container { get; set; } = new();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Register();
+            MainStartup();
+
+            base.OnStartup(e);
+        }
+
+        private void Register()
+        {
+            Container.RegisterSingleton<IMessenger, Messenger>();
+            Container.RegisterSingleton<MainViewModel>();
+            Container.RegisterSingleton<HomeViewModel>();
+            Container.RegisterSingleton<LoginViewModel>();
+            Container.RegisterSingleton<RegistrationViewModel>();
+        }
+        private void MainStartup()
+        {
+            var mainView = new MainView();
+            mainView.DataContext = Container?.GetInstance<MainViewModel>();
+            mainView.ShowDialog();
+        }
+    }
+}
---
+++ ProblemApp/StoreApp/Services/Classes/NavigationService.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Views;
+using StoreApp.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Services.Classes
+{
+    internal class NavigationService : INavigationService
+    {
+        private readonly IMessenger _messenger;
+
+        public NavigationService(IMessenger messenger)
+        {
+            _messenger = messenger;
+        }
+
+        public void NavigateTo<T>() where T : ViewModelBase
+        {
+            _messenger.Send(new NavigationMessage()
+            {
+                ViewModelType = typeof(T)
+            });
+        }
+    }
+}
