4e929d6a82b54013b93c5b8226da1ddd2ed113c4
Farkhadzz <zulfugarovfarhad6@gmail.com>
2023-04-04 03:04:26 +0400

    Added DeadLine Works

---
+++ DeadLine (6.03)/WpfApp3/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp3.Services.Classes;
+
+namespace WpfApp3.ViewModel
+{
+    public class MainViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateService(this);
+        }
+    }
+}
