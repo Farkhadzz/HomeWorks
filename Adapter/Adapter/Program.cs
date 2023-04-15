d6988416b3f07e6ed4843decac9f351610f2c541
Farkhadzz <zulfugarovfarhad6@gmail.com>
2023-04-15 12:50:53 +0400

    Added Patterns

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Transactions;
+
+ISpeed kmspeed = new kmSpeed(60.0f);
+ISpeed mlspeed = new mlSpeed(60.0f);
+
+Console.WriteLine(kmspeed.GetSpeed());
+Console.WriteLine(mlspeed.GetSpeed());
+
+
+
+
+
+interface ISpeed
+{
+    float GetSpeed();
+    void AdJust();
+}
+
+class kmSpeed : ISpeed
+{
+    private float currentSpeed;
+    public kmSpeed(float sp)
+    { 
+        currentSpeed = sp;
+    }
+    public void AdJust()
+    {
+        Console.WriteLine("KM Adjustment");
+    }
+    public float GetSpeed()
+    {
+        return currentSpeed;
+    }
+}
+
+class mlSpeed
+{
+    private float currentSpeed;
+    public mlSpeed(float sp)
+    {
+        currentSpeed = sp;
+    }
+    public float GetSpeed(float sp)
+    {
+        return currentSpeed;
+    }
+    protected void AdJust()
+    {
+        Console.WriteLine("ML Adjustment");
+    }
+}
+class AdapterForMlSpeed : mlSpeed , ISpeed
+{
+    public AdapterForMlSpeed(float sp) : base(sp) { }
+    float ISpeed.GetSpeed()
+    {
+        return base.GetSpeed(60) * 0.6f;
+    }
+    void ISpeed.AdJust()
+    {
+        base.AdJust();
+        Console.WriteLine("İn The Adjust() Method");
+    }
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Threading.Channels;
+
+ISite mySite = new SiteProxy(new Site());
+
+Console.WriteLine(mySite.GetPage(1));
+Console.WriteLine(mySite.GetPage(2));
+Console.WriteLine(mySite.GetPage(3));
+
+Console.WriteLine(mySite.GetPage(2));
+
+
+interface ISite
+{
+    string GetPage(int num);
+}
+
+class Site : ISite
+{
+    public string GetPage(int num)
+    {
+        return "This Page : " + num;
+    }
+}
+
+class SiteProxy : ISite
+    {
+        private ISite _site;
+        private Dictionary<int, string> cache;
+        
+        public SiteProxy(ISite site)
+        {
+            this._site = site;
+            cache = new Dictionary<int, string>();
+        }
+        public string GetPage(int num)
+        {
+            string page;
+            if (cache.ContainsKey(num))
+            {
+                page = cache[num];
+                page = "From Cache : " + page; 
+            }
+            else
+            {
+                page = _site.GetPage(num);
+                cache.Add(num, page);
+            }
+            return page;
+        }
+
+}
