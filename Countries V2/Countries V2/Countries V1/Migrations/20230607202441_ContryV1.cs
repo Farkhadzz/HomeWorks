61a4a656d8e87b9e0c9e1edd1d4219ce03942a34
Farkhadzz <zulfugarovfarhad6@gmail.com>
2023-06-23 11:17:00 +0400

    Added Countries V2

---
+++ Countries V2/Countries V2/Countries V1/Migrations/20230607202441_ContryV1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Countries_V1.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContryV1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearCreated = table.Column<int>(type: "int", nullable: false),
+                    GovernmentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MapImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Population = table.Column<long>(type: "bigint", nullable: false),
+                    Area = table.Column<double>(type: "float", nullable: false),
+                    GDP = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
---
+++ Countries V2/Countries V2/Countries V1/Migrations/20230623065237_Add-Migration SecondUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Countries_V1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMigrationSecondUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Anthem",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Leader",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Anthem",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "Leader",
+                table: "Countries");
+        }
+    }
+}
